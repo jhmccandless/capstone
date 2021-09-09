04b519c4ecb106d213a6dc831423d4ebb6549650
Jason McCandless <jhmccandless1105@gmail.com>
2021-09-09 16:33:15 -0500

    working on the form

--- src/components/NewGameParamsUI.jsx
+++ src/components/NewGameParamsUI.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function NewGameParamsUI({ gameParameters }) {
+  const [selectedOption, setSelectedOption] = useState("standard");
+
+  // console.log(selectedOption);
+
+  function handleChange(event) {
+    console.log("changed evnet");
+    setSelectedOption(event.target.value);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    // console.log(event.target[0].value);
-    // console.log(event.target[1].value);
-    // console.log(event.target[2].value);
-    // console.log(event.target[3].value);
-    // console.log(event.target[4].value);
-    // console.log(event.target[5].value);
-    gameParameters(78);
+    let gamePlayDetails = [];
+    console.log(event.target[0].value);
+    console.log(event.target[1].value);
+    console.log(event.target[2].value);
+    console.log(selectedOption);
+    console.log(gamePlayDetails);
+    // gameParameters();
 
     // console.log("submitted");
     // console.log(event.target.value);
@@ -29,13 +39,44 @@ function NewGameParamsUI({ gameParameters }) {
           <br />
           <label htmlFor="max_score">Score Goal:</label>
           <input type="number" name="max_score" defaultValue="100"></input>
-          <br />
+          {/* <br />
           <input type="radio" id="standard" name="game-type" defaultChecked />
           <label htmlFor="standard">Standard</label>
           <input type="radio" id="two-dice" name="game-type" />
           <label htmlFor="two-dice">Two-Dice Pig</label>
           <input type="radio" id="big-pig" name="game-type" />
-          <label htmlFor="big-pig">Big Pig</label>
+          <label htmlFor="big-pig">Big Pig</label> */}
+          <br />
+          <label>
+            <input
+              type="radio"
+              value="standard"
+              checked={selectedOption === "standard"}
+              onChange={handleChange}
+            />
+            Standard
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value="two-dice"
+              checked={selectedOption === "two-dice"}
+              onChange={handleChange}
+            />
+            Two-Dice Pig
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value="big-pig"
+              checked={selectedOption === "big-pig"}
+              onChange={handleChange}
+            />
+            Big Pig
+          </label>
+
           <br />
           <button type="submit">Let's Play!</button>
         </form>
