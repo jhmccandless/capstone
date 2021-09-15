389dacd42c4543f6d363fa84d49d40e225b32ca9
Jason McCandless <jhmccandless1105@gmail.com>
2021-09-14 20:01:27 -0500

    tried to input fonts, decided to work on the score table. have it working, but needs styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import "./styling/App.css";
 import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter, Link, Switch, Route, Redirect } from "react-router-dom";
+import { Box } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import { createTheme } from "@material-ui/core/styles";
 
 import MainGame from "./containers/MainGame";
 import HomePageUI from "./components/HomePageUI";
@@ -11,41 +15,49 @@ import GameRulesUI from "./components/GameRulesUI";
 import ScoreboardUI from "./components/ScorboardUI";
 import PostGame from "./containers/PostGame";
 
+const theme = createTheme({});
+
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <div className="App">
-          <header className="App-header">
-            <h1>Porcine</h1>
-            <div className="link-div">
-              <Link className="header-link" to="/homepage">
-                Home
-              </Link>
-              <Link className="header-link" to="/new_game_setup">
-                New Game
-              </Link>
-              <Link className="header-link" to="/game_rules">
-                Game Rules
-              </Link>
-              <Link className="header-link" to="/scoreboard">
-                Scoreboard
-              </Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <BrowserRouter>
+            <div className="App">
+              <header className="App-header">
+                <Box>
+                  <h1>Porcine</h1>
+                </Box>
+                <div className="link-div">
+                  <Link className="header-link" to="/homepage">
+                    Home
+                  </Link>
+                  <Link className="header-link" to="/new_game_setup">
+                    New Game
+                  </Link>
+                  <Link className="header-link" to="/game_rules">
+                    Game Rules
+                  </Link>
+                  <Link className="header-link" to="/scoreboard">
+                    Scoreboard
+                  </Link>
+                </div>
+              </header>
+              <Switch>
+                <Route exact path="/homepage" component={HomePageUI} />
+                <Route path="/new_game_setup" component={NewGameParams} />
+                <Route path="/current_game" component={MainGame} />
+                <Route path="/game_rules" component={GameRulesUI} />
+                <Route path="/scoreboard" component={ScoreboardUI} />
+                <Route path="/post_game" component={PostGame} />
+                <Route path="*">
+                  <Redirect to="/homepage" />
+                </Route>
+              </Switch>
             </div>
-          </header>
-          <Switch>
-            <Route exact path="/homepage" component={HomePageUI} />
-            <Route path="/new_game_setup" component={NewGameParams} />
-            <Route path="/current_game" component={MainGame} />
-            <Route path="/game_rules" component={GameRulesUI} />
-            <Route path="/scoreboard" component={ScoreboardUI} />
-            <Route path="/post_game" component={PostGame} />
-            <Route path="*">
-              <Redirect to="/homepage" />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+          </BrowserRouter>
+        </CssBaseline>
+      </ThemeProvider>
     </Provider>
   );
 }
--- src/components/ScorboardUI.jsx
+++ src/components/ScorboardUI.jsx
@@ -18,19 +18,36 @@ function ScoreboardUI() {
     });
     return () => (mounted = false);
   }, []);
-  // console.log(gettingData());
 
   return (
     <>
       <div>this is the the scoreboard</div>
-      <ul>
-        {scores.map((scores, index) => (
-          <li key={index}>
-            {scores.username} Type: {scores.game_type} score: {scores.own_score}{" "}
-            percentage: {scores.percentage}
-          </li>
-        ))}
-      </ul>
+      <div className="scoreTable">
+        <table id="cal-table">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Game Type</th>
+              <th>Score</th>
+              <th>Opponent's Score</th>
+              <th>Game Goal</th>
+              <th>Percentage Won By</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scores.map((el) => (
+              <tr>
+                <th>{el.username}</th>
+                <th>{el.game_type}</th>
+                <th>{el.own_score}</th>
+                <th>{el.opponent_score}</th>
+                <th>{el.game_goal}</th>
+                <th>{el.percentage}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
