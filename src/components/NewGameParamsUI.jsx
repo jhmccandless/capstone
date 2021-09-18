41c678ad1f5e3b25126925466b35cc37e1e3f0f1
Jason McCandless <jhmccandless1105@gmail.com>
2021-09-18 12:16:41 -0500

    have most of the styling done

--- src/components/NewGameParamsUI.jsx
+++ src/components/NewGameParamsUI.jsx
@@ -3,6 +3,22 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "../styling/NewGameParams.css";
 
+import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+
+const HtmlTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "rgb(255, 139, 216)",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 220,
+    fontSize: "inherit",
+    border: "3px solid white",
+    boxShadow: "4px 4px white",
+  },
+}));
+
 function NewGameParamsUI({ gameParameters }) {
   let history = useHistory();
 
@@ -56,38 +72,101 @@ function NewGameParamsUI({ gameParameters }) {
             <br />
             <br />
             <h3>Game Style:</h3>
+            <HtmlTooltip
+              placement="right-end"
+              title={
+                <React.Fragment>
+                  <div className="rule-snippet">
+                    Key Rules:
+                    <ul>
+                      <li>
+                        Rolling a one clears Current Total and changes player
+                        turn
+                      </li>
+                    </ul>
+                  </div>
+                </React.Fragment>
+              }
+            >
+              <label className="radio">
+                <input
+                  type="radio"
+                  value="standard"
+                  checked={selectedOption === "standard"}
+                  onChange={handleChange}
+                />
+                Standard
+              </label>
+            </HtmlTooltip>
 
-            <label className="radio">
-              <input
-                type="radio"
-                value="standard"
-                checked={selectedOption === "standard"}
-                onChange={handleChange}
-              />
-              Standard
-            </label>
             <br />
-
-            <label className="radio">
-              <input
-                type="radio"
-                value="two-dice"
-                checked={selectedOption === "two-dice"}
-                onChange={handleChange}
-              />
-              Two-Dice Pig
-            </label>
+            <HtmlTooltip
+              placement="right"
+              title={
+                <React.Fragment>
+                  <div className="rule-snippet">
+                    Key Rules:
+                    <ul>
+                      <li>
+                        Rolling a one on either dice clears the Current Total
+                        and changes player turn
+                      </li>
+                      <li>
+                        Rolling a one on both dice clears the Current Total as
+                        well as the current player's score and changes the
+                        player's turn
+                      </li>
+                      <li>
+                        If a double is rolled, you have to roll again (unless it
+                        is a double one)
+                      </li>
+                    </ul>
+                  </div>
+                </React.Fragment>
+              }
+            >
+              <label className="radio">
+                <input
+                  type="radio"
+                  value="two-dice"
+                  checked={selectedOption === "two-dice"}
+                  onChange={handleChange}
+                />
+                Two-Dice Pig
+              </label>
+            </HtmlTooltip>
             <br />
-
-            <label className="radio">
-              <input
-                type="radio"
-                value="big-pig"
-                checked={selectedOption === "big-pig"}
-                onChange={handleChange}
-              />
-              Big Pig
-            </label>
+            <HtmlTooltip
+              placement="right-start"
+              title={
+                <React.Fragment>
+                  <div className="rule-snippet">
+                    Key Rules:
+                    <ul>
+                      <li>
+                        Rolling a one on either dice clears the Current Total
+                        and changes player turn
+                      </li>
+                      <li>
+                        Rolling a double adds twice the amount showed from both
+                        dice to the current total (double 1 adds 25)
+                      </li>
+                      <li>If a double is rolled, you have to roll again</li>
+                    </ul>
+                  </div>
+                </React.Fragment>
+              }
+            >
+              <label className="radio">
+                <input
+                  type="radio"
+                  value="big-pig"
+                  checked={selectedOption === "big-pig"}
+                  onChange={handleChange}
+                />
+                Big Pig
+              </label>
+            </HtmlTooltip>
             <br />
             <br />
             <button type="submit">
